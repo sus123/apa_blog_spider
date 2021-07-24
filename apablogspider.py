ea452c6fd09e49ebab7c2a4951649d90ec7c7c23
Sushma <sushma.shrestha50@gmail.com>
2021-07-24 16:29:41 +1000

    initial commit

---
+++ apablogspider.py
@@ -0,0 +1,13 @@
+import scrapy
+
+
+class ApaBlogSpider(scrapy.Spider):
+    name = 'apaspider'
+    start_urls = ['https://blog.apastyle.org/apastyle/website/']
+
+    def parse(self, response, **kwargs):
+        for title in response.css('.entry-header'):
+            yield {'title': title.css('a::text').get()}
+
+        for next_page in response.css('.pager-right > a'):
+            yield response.follow(next_page, self.parse)
